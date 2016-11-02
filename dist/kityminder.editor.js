dbe11909e42852ecd088369381b8291acaa22631
grzhan <envy518@gmail.com>
2016-11-02 23:00:48 +0800

    patch: export and import ui

--- dist/kityminder.editor.js
+++ dist/kityminder.editor.js
@@ -1,6 +1,6 @@
 /*!
  * ====================================================
- * kityminder-editor - v1.0.56 - 2016-07-06
+ * kityminder-editor - v1.0.56 - 2016-11-02
  * https://github.com/fex-team/kityminder-editor
  * GitHub: https://github.com/fex-team/kityminder-editor 
  * Copyright (c) 2016 ; Licensed 
@@ -901,7 +901,7 @@ _p[12] = {
                     }
                 });
                 minder.on("dblclick", function() {
-                    if (minder.getSelectedNode()) {
+                    if (minder.getSelectedNode() && minder._status !== "readonly") {
                         editText();
                     }
                 });
